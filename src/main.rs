eb7b66eb86fd9812357af588423b051d36a2de31
コマリン親衛隊 <contact@tuna2134.dev>
2024-02-03 03:02:49 +0000

    Add shard to AppState struct and update presence in Ready event

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::{env, sync::Arc};
+use tokio::sync::RwLock;
 use twilight_cache_inmemory::{InMemoryCache, ResourceType};
 use twilight_gateway::{Event, Intents, Shard, ShardId};
 use twilight_http::Client as HttpClient;
-use twilight_model::{channel::message::AllowedMentions, http::attachment::Attachment};
+use twilight_model::{channel::message::AllowedMentions, gateway::{payload::outgoing::UpdatePresence, presence::{Activity, ActivityType, Status}}, http::attachment::Attachment};
 
 struct AppState {
     http: HttpClient,
     cache: InMemoryCache,
+    shard: RwLock<Shard>,
 }
 
 #[tokio::main]
@@ -17,14 +19,14 @@ async fn main() -> anyhow::Result<()> {
     let token: String = env::var("DISCORD_TOKEN")?;
 
     let intents: Intents = Intents::GUILD_MESSAGES | Intents::GUILDS | Intents::MESSAGE_CONTENT;
-    let mut shard: Shard = Shard::new(ShardId::ONE, token.clone(), intents);
+    let shard: RwLock<Shard> = RwLock::new(Shard::new(ShardId::ONE, token.clone(), intents));
     let http: HttpClient = HttpClient::new(token.clone());
     let cache: InMemoryCache = InMemoryCache::builder()
         .resource_types(ResourceType::CHANNEL)
         .build();
-    let state: Arc<AppState> = Arc::new(AppState { http, cache });
+    let state: Arc<AppState> = Arc::new(AppState { http, cache, shard });
     loop {
-        let event: Event = match shard.next_event().await {
+        let event: Event = match state.shard.write().await.next_event().await {
             Ok(event) => event,
             Err(error) => {
                 log::error!("Error receiving event: {:?}", error);
@@ -44,6 +46,33 @@ async fn handle_event(state: Arc<AppState>, event: Event) -> anyhow::Result<()> 
     match event {
         Event::Ready(_) => {
             log::info!("The bot is ready!");
+            state.shard.write().await.command(
+                &UpdatePresence::new(
+                    vec![
+                        Activity {
+                            application_id: None,
+                            assets: None,
+                            buttons: Vec::new(),
+                            created_at: None,
+                            details: None,
+                            emoji: None,
+                            flags: None,
+                            id: None,
+                            instance: None,
+                            kind: ActivityType::Playing,
+                            name: format!("v{}", env!("CARGO_PKG_VERSION")),
+                            party: None,
+                            secrets: None,
+                            state: None,
+                            timestamps: None,
+                            url: None,
+                        }
+                    ],
+                    true,
+                    None,
+                    Status::Online,
+                )?
+            ).await?;
         }
         Event::MessageCreate(message) => {
             if message.author.bot {
